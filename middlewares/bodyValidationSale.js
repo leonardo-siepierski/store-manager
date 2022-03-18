68ab498ab86263358afba92675a1b72e60cb7eca
leonardo <leonardo.siepierski@outlook.com>
2022-03-18 18:17:40 -0300

    Minor fix in next function on validation  middleware

--- middlewares/bodyValidationSale.js
+++ middlewares/bodyValidationSale.js
@@ -1,9 +1,12 @@
 const idValidation = (req, res, next) => {
   try {
     const [{ productId }] = req.body;
+
     if (!productId) {
       return res.status(400).json({ message: '"productId" is required' });
     }
+
+    next();
   } catch (err) {
     next(err);
   }
