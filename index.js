4b5cb7b3c1bf6626ee7862242e124397de5fec45
leonardo <leonardo.siepierski@outlook.com>
2022-03-18 15:20:16 -0300

    Initial config. Error and connection created

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+// const productsRouter = require('./routes/productsRouter');
+// const salesRouter = require('./routes/salesRouter');
+// const error = require('./middlewares/error');
+
+const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
