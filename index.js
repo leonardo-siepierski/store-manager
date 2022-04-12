f7d108bd4b36533638e2336f495a6e13f1774381
leonardo <leonardo.siepierski@outlook.com>
2022-04-12 15:49:16 -0300

    Project changed for personal github

--- index.js
+++ index.js
@@ -12,15 +12,9 @@ app.use('/products', productsRouter);
 app.use('/sales', salesRouter);
 app.use(error);
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
-// Boa parte das dúvidas que tive sobre o funcionamento e a lógica do projeto
-// foram tiradas das gravações das aulas ao vivo e dos repos abaixo
-// https://github.com/tryber/sd-015-a-store-manager/pull/122
-// https://github.com/tryber/sd-015-a-store-manager/pull/104
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
